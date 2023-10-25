1ac8d88f14f7965901f4f694a1b9585666e610c9
chichu <1669738329@qq.com>
2023-10-25 09:46:04 +0800

    first

---
+++ 3.1.py
@@ -0,0 +1,6 @@
+# 郭鑫 20222146060 数据科学与大数据技术
+year = int(input("请输入年份："))
+if year % 400 == 0 or (year % 4 == 0 and year % 100 != 0):
+    print("{0}是闰年".format(year))
+else:
+    print("{0}不是闰年".format(year))
