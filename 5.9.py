d65b7b3de6d4402931fe15549b4ed2a83da2ce83
chichu <1669738329@qq.com>
2023-10-31 19:58:40 +0800

    ti_jiao

---
+++ 5.9.py
@@ -0,0 +1,18 @@
+# 郭鑫 20222146060 数据科学与大数据技术
+def f(n):
+    count = 0
+    a = []
+    for i in range(1, n):
+        if n % i == 0:
+            count += i
+            a.append(i)
+    if count == n:
+        return 1, a
+    else:
+        return 0
+
+
+for x in range(1, 1000):
+    if f(x):
+        print("sum({})".format(f(x)[1]), end=' = ')
+        print(x)
