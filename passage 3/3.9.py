69280a4c78e86701802afad8bce73c3126b95a05
chichu <1669738329@qq.com>
2024-01-08 21:13:48 +0800

    ti_jiao

--- passage 3/3.9.py
+++ passage 3/3.9.py
@@ -1,11 +1,18 @@
 # 郭鑫 2222146060 数据科学与大数据技术
+# 回文数
+def f(x):
+    x = str(x)
+    a = [n for n in x]
+    return a
+
+
 count = 0
 for i in range(1000, 10000):
-    x1 = i // 1000
-    x2 = (i % 1000) // 100
-    x3 = (i % 100) // 10
-    x4 = i % 10
-    if x1 == x4 and x2 == x3:
+    # x1 = i // 1000
+    # x2 = (i % 1000) // 100
+    # x3 = (i % 100) // 10
+    # x4 = i % 10
+    if f(i)[0] == f(i)[3] and f(i)[1] == f(i)[2]:
         count += 1
         print(i, end='\t')
         if count % 10 == 0:
